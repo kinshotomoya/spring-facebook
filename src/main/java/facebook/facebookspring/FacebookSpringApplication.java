a3bd8f96acc9b706992568fcefbf9e32015d29d7
金正朋也 <mhtnim1023@gmail.com>
2019-07-08 21:49:02 +0900

    buildspec

---
+++ src/main/java/facebook/facebookspring/FacebookSpringApplication.java
@@ -0,0 +1,15 @@
+package facebook.facebookspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+
+
+@SpringBootApplication
+public class FacebookSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacebookSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/facebook/facebookspring/FacebookSpringApplicationTests.java
@@ -0,0 +1,16 @@
+package facebook.facebookspring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FacebookSpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
